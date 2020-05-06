c55d0bc62ecac1325e72794127bc517a023a59c0
Henry Le <henryle.b11@gmail.com>
2020-05-06 17:50:21 -0500

    fix lev 2 js

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -42,7 +42,7 @@ function init_cap(str) {
 function makeTable (tblData) {
     tblData.forEach(obj => {
         // print out the retrieve object
-        console.log("making table...", obj); 
+        // console.log("making table...", obj); 
         
         // add row for each object in the array
         var row = tbody.append("tr");
@@ -51,19 +51,19 @@ function makeTable (tblData) {
         // of the same row
         Object.entries(obj).forEach(([key, value]) => {
         
-            // if it is city, state, country : capitalize all letters
-            if (key == "state" || key == "country"){
-                value = value.toUpperCase();
-            }
+            // // if it is city, state, country : capitalize all letters
+            // if (key == "state" || key == "country"){
+            //     value = value.toUpperCase();
+            // }
 
-            // if it is city or shape, only cap initials 
-            else if (key == "city"  || key == "shape"){
-                value = init_cap(value);
-            }
+            // // if it is city or shape, only cap initials 
+            // else if (key == "city"  || key == "shape"){
+            //     value = init_cap(value);
+            // }
 
-            // others left alone
-            else
-            value = value;
+            // // others left alone
+            // else
+            // value = value;
 
             // add tag td for data 
             var cell = row.append("td");
@@ -83,23 +83,6 @@ makeTable(events)
 // Select the date time input form
 var dtForm = d3.select("#datetime");
 
-// Select the city input form
-var ctForm = d3.select("#city");
-
-// Select the state input form
-var stForm = d3.select("#state");
-
-// Select the country input form
-var ctrForm = d3.select("#country");
-
-// Select the shape input form
-var shpForm = d3.select("#shape");
-
-
-var keyList = Object.keys(data[0]);
-console.log(keyList);
-
-
 // id of all input boxes
 // var idArr = ["#datetime", "#city", "#state", "#country", "#shape"];
 var idArr = {
@@ -110,35 +93,27 @@ var idArr = {
     shape : "#shape"
 };
 
-// create function to get value from the input box
-function getInput (id) {
-    outVal = d3.select(id).property("value");
-    return outVal; 
-};
 
 // loop and get a object of filtering parameters input by user
 var inpVal;  // each input from the form
 var inpValArr = {};  // create an object to hold the key-value pairs
 // ---------------------------- //
-Object.entries(idArr).forEach(([key, value]) => {
-    console.log(value);
-    // get the user input value from the UI form 
-    inpVal = getInput(value);
-    console.log(`getting input val for ${key, value}:${inpVal}`);
-    if (inpVal != ""){
-        console.log(`valid input val is ${key, inpVal}`);
-        inpVal = inpVal;
-    }
+function getAllInpt(){
+    Object.entries(idArr).forEach(([key, value]) => {
+        console.log(value);
+        // get the user input value from the UI form 
+        inpVal = d3.select(value).property("value"); //getInput(value);
+        console.log(`getting input val for ${key, value}:${inpVal}`);
 
-    inpValArr[key] = inpVal; // push the key with value into object
-});
+        if (inpVal != ""){
+            console.log(`valid input val is ${key, inpVal}`);
+            inpValArr[key] = inpVal; // push the key with value into object
+        }
+        else
+            inpValArr = inpValArr;        
+    });
+}
 
-console.log("inpValArr is : ", inpValArr);
-
-// for (eK; eK <= keyList.length, eK++) {
-//     var filterData = events.filter(event => event.datetime == dateInput);
-//     console.log(filterData);
-// };
 
 
 function exeFilter() {
@@ -174,13 +149,21 @@ function filterData(event) {
    return Object.keys(this).every(key => event[key] == this[key]);
 };
 
+var ftrD;
 function exeMultiFilter () {
     // Prevent the page from refreshing
-    d3.event.preventDefault();
+    // d3.event.preventDefault();
 
     // clear old table
     tbody.html("");
     
+    getAllInpt();
+
+
+    if (Object.keys(inpValArr).length !== 0) { 
     // filter all conditions and return the final data
-    var ftrD = events.filter(filterData, inpValArr);
+    ftrD = events.filter(filterData, inpValArr);
+    makeTable(ftrD);}
+    else
+        makeTable(events);
 }
